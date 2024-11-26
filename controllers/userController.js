b9873034d891e034f56df755a3e3c7008699a4f3
unknown <email id>
2024-11-26 15:53:22 +0300

    github workflows

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,8 @@ exports.getUser = async (req, res) => {
     const user = await User.findById(req.params.id).select("-password");
     if (!user) {
       return res.status(404).json({ message: "User not found" });
-    }
-    res.json(user);
+    
+    res.json(user); 
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
